31cb15e7d9f98595fc7d9d7eec053b0125aac88d
NaraeJang <j.naaaarae@gmail.com>
2023-09-27 18:32:54 -0400

    add item

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
 import Form from "./Form";
+import { nanoid } from "nanoid";
+import { ToastContainer, toast } from "react-toastify";
+
 const App = () => {
   const [items, setItems] = useState([]);
 
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+
+    setItems([...items, newItem]);
+    toast.success("successfully added an item");
+  };
+
   return (
     <section className="section-center">
-      <Form />
+      <ToastContainer position="top-center" />
+      <Form addItem={addItem} />
     </section>
   );
 };
