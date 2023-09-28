1ec3039e7a97b332b476fc10a63a35945663112a
NaraeJang <j.naaaarae@gmail.com>
2023-09-27 21:07:45 -0400

    edit item

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ import { nanoid } from "nanoid";
 import { ToastContainer, toast } from "react-toastify";
 import Items from "./Items";
 
-const getLocalStorage = () => {
-  let list = localStorage.getItem("list");
+// const getLocalStorage = () => {
+//   let list = localStorage.getItem("list");
 
-  if (list) {
-    JSON.parse(localStorage.getItem("list"));
-  } else {
-    list = [];
-  }
+//   if (list) {
+//     JSON.parse(localStorage.getItem("list"));
+//   } else {
+//     list = [];
+//   }
 
-  return list;
-};
+//   return list;
+// };
 const setLocalStorage = (items) => {
   localStorage.setItem("list", JSON.stringify(items));
 };
 
 const defaultList = JSON.parse(localStorage.getItem("list")) || [];
 const App = () => {
-  getLocalStorage();
+  // getLocalStorage();
   const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
@@ -43,11 +43,23 @@ const App = () => {
     toast.success("Deleted the item");
   };
 
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setItems(newItems);
+    setLocalStorage(newItems);
+  };
+
   return (
     <section className="section-center">
       <ToastContainer position="top-center" />
       <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem} />
+      <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
   );
 };
